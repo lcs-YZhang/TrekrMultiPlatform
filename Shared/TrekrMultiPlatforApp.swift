dcea2bed2cd3cc89e477f42575833ded68fa9f0b
Steven Zhang <YZhang@lcs.on.ca>
2022-01-18 09:20:50 -0500

    Initial Commit

---
+++ Shared/TrekrMultiPlatforApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrMultiPlatforApp.swift
+//  Shared
+//
+//  Created by Steven Zhang on 2022-01-18.
+//
+
+import SwiftUI
+
+@main
+struct TrekrMultiPlatforApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
