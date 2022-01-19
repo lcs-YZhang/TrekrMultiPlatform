f358b935b1f27d65c8ede8bfd8b0460a929e2108
Steven Zhang <YZhang@lcs.on.ca>
2022-01-19 10:07:22 -0500

    Add LocationStore to load locations from JSON file

---
+++ Shared/Model/LocationStore.swift
@@ -0,0 +1,25 @@
+//
+//  LocationStore.swift
+//  TrekrMultiPlatform
+//
+//  Created by Steven Zhang on 2022-01-19.
+//
+
+import Foundation
+class LocationStore: ObservableObject {
+    
+    var places: [Location]
+    
+    init() {
+        
+        let url = Bundle.main.url(forResource: "locations", withExtension: "json")!
+        
+        let data = try! Data(contentsOf: url)
+        
+        places = try! JSONDecoder().decode([Location].self, from: data)
+        
+        places.sort(by: {
+            $0.name < $1.name
+        })
+    }
+}
