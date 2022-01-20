bd0d82a5215fde67694b29a10d6c391cf295a286
Steven Zhang <YZhang@lcs.on.ca>
2022-01-20 10:09:09 -0500

    Added navigation link to the Map view

--- Shared/Views/WorldMap.swift
+++ Shared/Views/WorldMap.swift
@@ -10,13 +10,31 @@ import SwiftUI
 
 struct WorldMap: View {
     
+    @ObservedObject var store: LocationStore
+    
     @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 44.43922,
                                                                           longitude: -78.26571),
                                            span: MKCoordinateSpan(latitudeDelta: 40,
                                                                   longitudeDelta: 40))
     
     var body: some View {
-        Map(coordinateRegion: $region)
+        Map(coordinateRegion: $region, annotationItems: store.places) { location in
+            
+            MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: location.latitude,
+                                                             longitude: location.longitude)) {
+                
+                NavigationLink(destination: LocationDetail(location: location)) {
+                    
+                    Image(location.country)
+                        .resizable()
+                        .cornerRadius(10)
+                        .frame(width: 80, height: 40)
+                        .shadow(radius: 3)
+                    
+                }
+            }
+            
+        }
             .navigationTitle("Map")
         
     }
@@ -25,7 +43,7 @@ struct WorldMap: View {
 struct WorldMap_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            WorldMap()
+            WorldMap(store: testStore)
         }
     }
 }
